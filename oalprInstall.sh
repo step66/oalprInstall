3dd7ada4f5272cd32fcddfc2abfba6ea57427e3b
step66 <red.mailhub@gmail.com>
2017-05-07 09:55:51 +0800

    Create oalprInstall.sh

---
+++ oalprInstall.sh
@@ -0,0 +1,4 @@
+#!/bin/bash -x
+# Change permissions first with: chmod 0755 oalprInstall.sh
+# Then run with: ./oalprInstall.sh
+sudo apt-get install -y openalpr openalpr-daemon openalpr-utils libopenalpr-dev
